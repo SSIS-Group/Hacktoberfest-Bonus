a4fd958e9286ceb5467d5592b31b6203086edcca
Akash Bobba <bobba.akash@gmail.com>
2019-10-10 21:47:44 -0400

    Update python3.py

--- python3/python3.py
+++ python3/python3.py
@@ -1,1 +1,15 @@
 print("Hello World")
+
+# Reading image meta-data (height width) from a directory of images
+from os import listdir
+from os.path import isfile, join
+from PIL import Image
+import os
+
+path = "path/"
+files = [f for f in listdir(path) if isfile(join(path, f))]
+images = []
+for filename in files:
+    im = Image.open(path+filename)
+    width, height = im.size 
+    images.append({'filename': filename, 'width':width, 'height': height})
