d27d8d3956373c0354b4c44f0982c370ccac4d3c
ed-archer <abin.xv@gmail.com>
2019-10-13 13:41:59 +0530

    Added folder 'Hackerearth' and solution to hackerearth problem- 'Goki and his breakup'

---
+++ Hackerearth/Goki and his breakup.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+ 
+{
+
+    int n,x,y;
+
+    cin>>n>>x;
+
+    while(n--)
+
+    {
+
+        cin>>y;
+
+        if(y>=x)
+
+        cout<<"YES"<<endl;
+
+        else
+
+        cout<<"NO"<<endl;
+
+    }
+
+}
